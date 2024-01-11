60ab950198948928e8226fc1169408d0e2611960
Simeon Petrov <simonpetrov85@gmail.com>
2024-01-11 16:13:09 +0200

    Update 04.js

--- Syntax, Functions and Statements-Ex/04.js
+++ Syntax, Functions and Statements-Ex/04.js
@@ -1,12 +1,10 @@
-function previousDay(num1, num2, num3) {
-      
-
-
-
-
-
-
-
+function previousDay(...arrgs) {
+      let [year, month, day] = arrgs;
+      const currentDate = new Date(year, month - 1, day - 1);
+      month = currentDate.getMonth();
+      year = currentDate.getFullYear();
+      day = currentDate.getDate();
+      return console.log(`${year}-${month += 1}-${day}`);
 }
-previousDay(2016, 9, 30);
+previousDay(2016, 3, 1);
 previousDay(2015, 5, 10);
