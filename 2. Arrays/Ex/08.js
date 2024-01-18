d8e7c4ac26c879afcbf7aaa7b146143b8a45f5f9
Simeon Petrov <simonpetrov85@gmail.com>
2024-01-18 18:54:22 +0200

    Create 08.js

---
+++ 2. Arrays/Ex/08.js
@@ -0,0 +1,15 @@
+function sortAnArrayByTwoCriteria(arr) {
+      arr.sort((a, b) => a.localeCompare(b));
+      for (let i = 0; i < arr.length; i++) {
+            for (let j = i + 1; j < arr.length; j++) {
+                  if (arr[i].length > arr[j].length) {
+                        const strToMove = arr.splice(j, 1);
+                        arr.splice(i, 0, ...strToMove);
+                  }
+            }
+      }
+
+      return arr.forEach(str => console.log(str));
+}
+sortAnArrayByTwoCriteria(['alpha', 'beta', 'gamma']);
+sortAnArrayByTwoCriteria(['Isacc', 'Theodor', 'Jack', 'Harrison', 'George']);
