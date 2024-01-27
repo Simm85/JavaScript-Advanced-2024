e8cfdae227d9c4ebadba345cf941ca150cd25266
Simeon Petrov <simonpetrov85@gmail.com>
2024-01-28 00:24:07 +0200

    Update SumNumbers.js

--- 4. DOM/Lab/03.Sum Numbers/SumNumbers.js
+++ 4. DOM/Lab/03.Sum Numbers/SumNumbers.js
@@ -1,3 +1,6 @@
-function calc() {
-    // TODO: sum = num1 + num2
+function calc(num1, num2, result) {
+    num1 = document.getElementById('num1').value;
+    num2 = document.getElementById('num2').value;
+    result = document.getElementById('sum');
+    return result.value = Number(num1) + Number(num2);
 }
