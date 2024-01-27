e0533fc9b8dd5d8953c5c9b6a6d7d06027610b05
Simeon Petrov <simonpetrov85@gmail.com>
2024-01-27 22:05:44 +0200

    Create 07.js

---
+++ 3. Objects and Composition/Ex/07.js
@@ -0,0 +1,32 @@
+function townsToJSON(arr) {
+      const strToArr = (str) => {
+            return str.split('|').filter(el => /\w/g.test(el)).map(el => el.trim());
+      }
+      const tableHeadings = strToArr(arr.shift());
+      const [Town, Latitude, Longitude] = tableHeadings;
+      const object = {
+            Town,
+            Latitude,
+            Longitude,
+      };
+
+      const result = [];
+
+      for (const el of arr) {
+            let [Town, Latitude, Longitude] = strToArr(el);
+            Latitude = Number(Latitude).toFixed(2);
+            Longitude = Number(Longitude).toFixed(2);
+            const newObject = Object.assign({}, object);
+            newObject.Town = Town;
+            newObject.Latitude = Number(Latitude);
+            newObject.Longitude = Number(Longitude);
+            result.push(newObject);
+      }
+
+      return console.log(JSON.stringify(result));
+}
+townsToJSON([
+      '| Town | Latitude | Longitude |',
+      '| Sofia | 42.696552 | 23.32601 |',
+      '| Beijing | 39.913818 | 116.363625 |'
+]);
