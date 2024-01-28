2cac9558e23146a5f39388b0ddf2a902cde779a6
Simeon Petrov <simonpetrov85@gmail.com>
2024-01-28 03:55:06 +0200

    Homework.

--- 4. DOM/Lab/06.Sum Table/SumTable.js
+++ 4. DOM/Lab/06.Sum Table/SumTable.js
@@ -1,3 +1,6 @@
 function sumTable() {
-
+      return document.getElementById('sum').textContent = Array.from(document.querySelectorAll('td'))
+            .filter(txt => /\d\./g.test(txt.textContent))
+            .map(txt => Number(txt.textContent))
+            .reduce((acc, n) => acc += n);
 }
--- 4. DOM/Lab/07.Extract Parenthesis/ExtractParenthesis.js
+++ 4. DOM/Lab/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,5 @@
 function extract(content) {
-
+      content = document.getElementById('content').textContent;
+      const patterns = [/[\(]{1}[\w\s\d]{1,}[\)]{1}/gm, /\(|\)/g];
+      return content.match(patterns[0]).map(el => el.replace(patterns[1], '')).join('; ');
 }
