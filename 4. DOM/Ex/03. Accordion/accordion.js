84992ee73c49da762bf36e10b96c3074de3ccb33
Simeon Petrov <simonpetrov85@gmail.com>
2024-01-28 17:07:37 +0200

    Homework.

--- 4. DOM/Ex/03. Accordion/accordion.js
+++ 4. DOM/Ex/03. Accordion/accordion.js
@@ -1,3 +1,12 @@
 function toggle() {
-    console.log('TODO:...');
+    const button = document.querySelector('.button');
+    const txtContainer = document.getElementById('extra');
+    if (button.textContent.includes('More')) {
+        button.textContent = 'Less';
+        txtContainer.style.display = 'block';
+        return;
+    }
+
+    button.textContent = 'More';
+    txtContainer.style.display = 'none';
 }
