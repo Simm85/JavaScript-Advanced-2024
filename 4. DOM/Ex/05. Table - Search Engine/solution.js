a08ef66e447917e3483a1b3e248d118530f94dd8
Simeon Petrov <simonpetrov85@gmail.com>
2024-01-29 21:37:17 +0200

    Update solution.js

--- 4. DOM/Ex/05. Table - Search Engine/solution.js
+++ 4. DOM/Ex/05. Table - Search Engine/solution.js
@@ -1,8 +1,32 @@
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
-   function onClick() {
-      //   TODO:
+   function onClick(userInput) {
+      const tableData = Array.from(document.querySelectorAll('tbody tr td'));
+      userInput = document.getElementById('searchField').value.toLowerCase();
 
+      if (userInput.length > 0) {
+         unselectRows(tableData);
+         selectRows(tableData, userInput);
+         return;
+      }
+
+      unselectRows(tableData);
+
+      function selectRows(array, input) {
+         array.forEach(el => {
+            if (el.textContent.toLowerCase().includes(input)) {
+               el.parentNode.classList.add('select');
+            }
+         });
+      }
+
+      function unselectRows(array) {
+         array.forEach(el => {
+            if (el.parentNode.classList.contains('select')) {
+               el.parentNode.classList.remove('select');
+            }
+         });
+      }
    }
 }
