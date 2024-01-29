41b8d51df7c1a8d639ff93c73ddbb160d09f9c6a
Simeon Petrov <simonpetrov85@gmail.com>
2024-01-29 14:09:47 +0200

    Update search.js

--- 4. DOM/Ex/04. Search in List/search.js
+++ 4. DOM/Ex/04. Search in List/search.js
@@ -1,3 +1,34 @@
 function search() {
-   // TODO
+   const towns = Array.from(document.getElementById('towns').children);
+   const searchBar = document.getElementById('searchText');
+   const result = document.getElementById('result');
+   const matchQuery = ((arr, inputString, counter) => {
+      if (inputString.length > 0) {
+         result.textContent = '';
+         inputString = inputString.toLowerCase();
+         arr.filter(el => el.textContent.toLowerCase().includes(inputString));
+         arr.forEach(el => {
+            el.style.fontWeight = '';
+            el.style.textDecoration = '';
+         });
+
+         arr.forEach(el => {
+            if (el.textContent.toLowerCase().includes(inputString)) {
+               counter++;
+               result.textContent = `${counter} matches found`;
+               el.style.fontWeight = 'bold';
+               el.style.textDecoration = 'underline';
+            }
+         });
+         return;
+      }
+
+      result.textContent = '';
+      arr.forEach(el => {
+         el.style.fontWeight = '';
+         el.style.textDecoration = '';
+      });
+   });
+
+   matchQuery(towns, searchBar.value, 0);
 }
