5b95846fc557729bd6d06c9f42cd68f841ed4239
Simeon Petrov <simonpetrov85@gmail.com>
2024-02-01 23:19:18 +0200

    Update app.js

--- 5. DOM Manipulations and Events/Lab/02. Delete-From-Table/app.js
+++ 5. DOM Manipulations and Events/Lab/02. Delete-From-Table/app.js
@@ -1,3 +1,18 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    const message = document.getElementById('result');
+    const inputField = document.querySelector('input');
+    const tableBody = document.querySelector('tbody');
+    const tableRows = Array.from(document.querySelectorAll('tbody tr'));
+    const mailPattern = /[\w\d_-]+@[\w\d_-]+[.]{1}[\w]{2,}/g;
+    Array.from(document.querySelectorAll('tbody tr td'))
+        .filter(el => mailPattern.test(el.textContent))
+        .map(el => el = el.textContent)
+        .forEach((el, i) => {
+            if (inputField.value.includes(el)) {
+                tableBody.removeChild(tableRows[i]);
+                message.textContent = 'Deleted.';
+            } else {
+                message.textContent = 'Not found.';
+            }
+        });
 }
