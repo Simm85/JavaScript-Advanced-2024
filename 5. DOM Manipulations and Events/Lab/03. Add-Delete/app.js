710c2be78c9c5c58fe291cd51980b22e5bc086bd
Simeon Petrov <simonpetrov85@gmail.com>
2024-02-02 13:30:03 +0200

    Update app.js

--- 5. DOM Manipulations and Events/Lab/03. Add-Delete/app.js
+++ 5. DOM Manipulations and Events/Lab/03. Add-Delete/app.js
@@ -1,3 +1,20 @@
 function addItem() {
-    //TODO...
+    const newItem = document.getElementById('newItemText');
+    const list = document.getElementById('items');
+    const li = document.createElement('li');
+    const delButton = document.createElement('a');
+
+    li.textContent = newItem.value;
+    delButton.href = '#';
+    delButton.textContent = '[Delete]';
+    li.appendChild(delButton);
+    list.appendChild(li);
+    list.addEventListener('click', event => removeItem(event));
+
+    function removeItem(event) {
+        if (event.target.tagName.toLowerCase() != 'a') {
+            return;
+        }
+        event.target.parentNode.remove();
+    }
 }
