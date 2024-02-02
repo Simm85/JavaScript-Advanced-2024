bf67afafef2abbd03ad180186d43de0af9c35bdf
Simeon Petrov <simonpetrov85@gmail.com>
2024-02-02 21:50:41 +0200

    Homework.

--- 5. DOM Manipulations and Events/Lab/05. Highlight-Active/app.js
+++ 5. DOM Manipulations and Events/Lab/05. Highlight-Active/app.js
@@ -1,3 +1,14 @@
 function focused() {
-    console.log('TODO:...');
+    const inputs = Array.from(document.querySelectorAll('input'));
+    const body = document.querySelector('body');
+    inputs.forEach(input => input.addEventListener('focus', e => focus(e)));
+    inputs.forEach(input => input.addEventListener('blur', e => blur(e)));
+
+    function focus(e) {
+        e.target.parentNode.className = 'focused';
+    }
+
+    function blur(e) {
+        e.target.parentNode.classList.remove('focused');
+    }
 }
