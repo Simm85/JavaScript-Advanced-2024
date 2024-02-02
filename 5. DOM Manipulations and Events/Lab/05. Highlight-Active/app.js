f0f3a875ec45453ba235ad2727ed59b1cfab0e51
Simeon Petrov <simonpetrov85@gmail.com>
2024-02-02 22:17:45 +0200

    Homework.

--- 5. DOM Manipulations and Events/Lab/05. Highlight-Active/app.js
+++ 5. DOM Manipulations and Events/Lab/05. Highlight-Active/app.js
@@ -1,6 +1,5 @@
 function focused() {
     const inputs = Array.from(document.querySelectorAll('input'));
-    const body = document.querySelector('body');
     inputs.forEach(input => input.addEventListener('focus', e => focus(e)));
     inputs.forEach(input => input.addEventListener('blur', e => blur(e)));
 
--- 5. DOM Manipulations and Events/Lab/06. Dynamic-Validation/app.js
+++ 5. DOM Manipulations and Events/Lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,13 @@
 function validate() {
-    console.log('TODO:...');
+    const pattern = /[a-z]+@[a-z]+\.[a-z]{2,3}/g;
+    const input = document.getElementById('email');
+    input.addEventListener('change', () => validate(input, pattern));
+
+    function validate(input, regEx) {
+        if (regEx.test(input.value)) {
+            input.classList.remove('error');
+            return;
+        }
+        input.className = 'error';
+    }
 }
