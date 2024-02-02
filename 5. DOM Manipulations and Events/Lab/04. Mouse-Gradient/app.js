33be2c0018db5762db5feb2d5bfeefb93e437989
Simeon Petrov <simonpetrov85@gmail.com>
2024-02-02 19:04:45 +0200

    Update app.js

--- 5. DOM Manipulations and Events/Lab/04. Mouse-Gradient/app.js
+++ 5. DOM Manipulations and Events/Lab/04. Mouse-Gradient/app.js
@@ -1,3 +1,13 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    const hoverArea = document.getElementById('gradient');
+    const result = document.getElementById('result');
+
+    hoverArea.addEventListener('mousemove', e => getCoords(e, result));
+    hoverArea.addEventListener('mouseout', () => result.textContent = '');
+
+    function getCoords(event, res) {
+        let mouseX = event.offsetX / (event.target.clientWidth - 1);
+        const percentage = Math.trunc(mouseX * 100);
+        res.textContent = percentage + '%';
+    }
 }
