4e6b7bc143aa8fe073e17ba8b30afa16a5344865
Simeon Petrov <simonpetrov85@gmail.com>
2024-02-05 19:54:35 +0200

    Update dropdown.js

--- 5. DOM Manipulations and Events/Ex/04. Fill-Dropdown/dropdown.js
+++ 5. DOM Manipulations and Events/Ex/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,12 @@
 function addItem() {
-    console.log('TODO:...');
+    const itemTxt = document.getElementById('newItemText');
+    const itemValue = document.getElementById('newItemValue');
+    const selectBar = document.getElementById('menu');
+
+    const option = document.createElement('option');
+    option.value = itemValue.value;
+    option.textContent = itemTxt.value;
+    selectBar.appendChild(option);
+    itemTxt.value = '';
+    itemValue.value = '';
 }
