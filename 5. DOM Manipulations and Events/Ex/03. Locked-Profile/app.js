7ee69e901f03c0004d7b6868e67882f92ec4e32b
Simeon Petrov <simonpetrov85@gmail.com>
2024-02-05 18:55:37 +0200

    Update app.js

--- 5. DOM Manipulations and Events/Ex/03. Locked-Profile/app.js
+++ 5. DOM Manipulations and Events/Ex/03. Locked-Profile/app.js
@@ -1,3 +1,25 @@
 function lockedProfile() {
-    console.log('TODO...')
+    Array.from(document.querySelectorAll('.profile')).forEach(profile => processProfile(profile.children));
+
+    function processProfile(array) {
+        const lockBtn = array[2];
+        const hiddenData = array[9];
+        const toogleBtn = array[10];
+        toogleBtn.addEventListener('click', showHideInfo);
+
+        function showHideInfo() {
+            if (lockBtn.checked) {
+                return;
+            }
+
+            if (toogleBtn.textContent == 'Show more') {
+                hiddenData.style.display = 'block';
+                toogleBtn.textContent = 'Hide it';
+                return;
+            }
+
+            hiddenData.style.display = 'none';
+            toogleBtn.textContent = 'Show more';
+        }
+    }
 }
