ff79b18058b39d16aba198b4baa2b0b7e90b5a89
Simeon Petrov <simonpetrov85@gmail.com>
2024-02-07 16:35:37 +0200

    Create 1. areaAndVolumeCalculator.js

---
+++ 6. Advanced Functions/Lab/1. areaAndVolumeCalculator.js
@@ -0,0 +1,23 @@
+function areaAndVolumeCalculator(areaFn, volFn, string) {
+      return JSON.parse(string).map(obj => {
+            return {
+                  area: areaFn.call(obj),
+                  volume: volFn.call(obj)
+            };
+      });
+}
+areaAndVolumeCalculator(area, vol, `[
+      {"x":"1","y":"2","z":"10"},
+      {"x":"7","y":"7","z":"10"},
+      {"x":"5","y":"2","z":"10"}
+      ]`
+);
+
+function area() {
+      return Math.abs(this.x * this.y);
+}
+
+function vol() {
+      return Math.abs(this.x * this.y * this.z);
+}
+
