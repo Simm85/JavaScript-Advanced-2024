ca941127ae06cb4c3b41f798194183a3d43e9a59
Simeon Petrov <simonpetrov85@gmail.com>
2024-02-12 16:31:53 +0200

    Create 5. commandProcessor.js

---
+++ 6. Advanced Functions/Lab/5. commandProcessor.js
@@ -0,0 +1,36 @@
+function commandProcessor() {
+      return {
+            string: null,
+            append(inputString) {
+                  if (this.string) {
+                        return this.string += inputString;
+                  }
+                  return this.string = inputString;
+            },
+            removeStart(n) {
+                  return this.string = this.string.substr(n, this.string.length);
+            },
+            removeEnd(n) {
+                  return this.string = this.string.substr(0, this.string.length - n);
+            },
+            print() {
+                  return console.log(this.string);
+            }
+      }
+}
+
+let firstZeroTest = commandProcessor();
+firstZeroTest.append('hello');
+firstZeroTest.append('again');
+firstZeroTest.removeStart(3);
+firstZeroTest.removeEnd(4);
+firstZeroTest.print();
+
+
+let secondZeroTest = commandProcessor();
+secondZeroTest.append('123');
+secondZeroTest.append('45');
+secondZeroTest.removeStart(2);
+secondZeroTest.removeEnd(1);
+secondZeroTest.print();
+
