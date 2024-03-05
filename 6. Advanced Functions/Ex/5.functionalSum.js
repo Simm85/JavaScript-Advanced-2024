93b598b1323d85e6431daac2d523b7cf7b4782f7
Simeon Petrov <simonpetrov85@gmail.com>
2024-03-05 21:00:40 +0200

    Homework.

---
+++ 6. Advanced Functions/Ex/5.functionalSum.js
@@ -0,0 +1,5 @@
+function functionalSum(num) {
+ 
+}
+functionalSum(1);
+functionalSum(1)(6);
---
+++ 6. Advanced Functions/Ex/6. monkeyPatcher.js
@@ -0,0 +1,93 @@
+function solution(arg) {
+      this.totalScore = null;
+      this.rating = null;
+      this.obfuscatedDownvotes = null;
+      this.obfuscatedUpvotes = null;
+
+      const upvote = () => {
+            this.upvotes++;
+      }
+
+      const downvote = () => {
+            this.downvotes++;
+      }
+
+      const obfuscateVotes = () => {
+            if (this.upvotes + this.downvotes > 50) {
+                  const numToAdd = 0.25 * Math.max(this.downvotes, this.upvotes);
+                  this.obfuscatedDownvotes = Math.ceil(this.downvotes + numToAdd);
+                  this.obfuscatedUpvotes = Math.ceil(this.upvotes + numToAdd);
+                  return true;
+            }
+            return false;
+      }
+
+      const defineRating = () => {
+            this.totalScore = this.upvotes - this.downvotes;
+            if (this.totalScore > 66) {
+                  this.rating = 'hot';
+            } else if (this.totalScore == 0 && this.upvotes + this.downvotes > 100) {
+                  this.rating = 'controversial';
+            } else if (this.totalScore < 0) {
+                  this.rating = 'unpopular';
+            }
+
+            if (this.upvotes + this.downvotes < 10 || !this.rating) {
+                  this.rating = 'new';
+            }
+      }
+
+      const report = () => {
+            let positiveVotes = this.upvotes;
+            let negativeVotes = this.downvotes;
+
+            if (obfuscateVotes()) {
+                  positiveVotes = this.obfuscatedUpvotes;
+                  negativeVotes = this.obfuscatedDownvotes;
+            }
+
+            defineRating();
+            return [positiveVotes, negativeVotes, this.totalScore, this.rating];
+      }
+
+      if (arg == 'upvote') {
+            upvote();
+      } else if (arg == 'downvote') {
+            downvote();
+      } else if (arg == 'score') {
+            return report();
+      }
+}
+
+const post = {
+      id: '3',
+      author: 'emil',
+      content: 'wazaaaaa',
+      upvotes: 100,
+      downvotes: 100
+};
+
+
+solution.call(post, 'upvote');
+solution.call(post, 'downvote');
+let score = solution.call(post, 'score');  // [127, 127, 0, 'controversial']
+
+for (let i = 0; i < 50; i++) {
+      solution.call(post, 'downvote'); // (executed 50 times)
+}
+score = solution.call(post, 'score');     // [139, 189, -50, 'unpopular']
+console.log(post);
+
+
+var forumPost = {
+      id: '1',
+      author: 'pesho',
+      content: 'hi guys',
+      upvotes: 0,
+      downvotes: 0
+};
+
+solution.call(forumPost, 'upvote');
+solution.call(forumPost, 'score');
+console.log(forumPost);
+
